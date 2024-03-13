c720681830d2eef77c3140ded5a5a9b8518c33f7
Kamil <kamilkwiecien9@gmail.com>
2024-03-13 22:05:18 +0100

    Weather api setup

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -4,18 +4,18 @@ import { useEffect, useState } from "react";
 const Weather = () => {
   const [data, setData] = useState([])
   const apiKey = "01b3faed431e892073487aa390b3e1a4";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=01b3faed431e892073487aa390b3e1a4"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&&units=metric&appid=01b3faed431e892073487aa390b3e1a4"
 
-  // useEffect(() => {
-  //   axios.get(url).then((res) => {
-  //     setData(res.data);
-  //   }).catch(err => {
-  //     console.log(err);
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    }).catch(err => {
+      console.log(err);
       
-  //   })
-  // },[setData])
+    })
+  },[setData])
 
-  // console.log(data);
+  console.log(data);
   
   return (
     <div className="weather">
