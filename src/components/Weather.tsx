dda62dab05ddbfbfd47c4b86cab73f06eacc184b
Kamil <kamilkwiecien9@gmail.com>
2024-03-14 18:39:43 +0100

    WeatherApi data fetching

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,27 +1,43 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
+
+type WeatherProps = {
+  name: string | undefined;
+  temp: number | undefined;
+  pressure: number | undefined;
+};
 
 const Weather = () => {
-  const [data, setData] = useState([])
-  const apiKey = "01b3faed431e892073487aa390b3e1a4";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&&units=metric&appid=01b3faed431e892073487aa390b3e1a4"
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&&units=metric&appid=01b3faed431e892073487aa390b3e1a4";
 
   useEffect(() => {
-    axios.get(url).then((res) => {
-      setData(res.data);
-    }).catch(err => {
-      console.log(err);
-      
-    })
-  },[setData])
+    const fetchWeather = async () => {
+      setIsLoading(true);
+      const res = await fetch(url);
+      const resData = (await res.json()) as WeatherProps[];
+      setData(resData);
+      setIsLoading(false);
+    };
+
+    fetchWeather();
+  }, []);
 
   console.log(data);
-  
+  if(isLoading) {
+    return <div className="weather">Loading...</div>
+  }
+
   return (
     <div className="weather">
-      
+      <h3>{data.name}</h3>
+      <p>{`${data.main.temp} °C`}</p>
+      <p>{`${data.main.pressure} hPa`}</p>
     </div>
   )
-}
+};
 
-export default Weather
+export default Weather;
